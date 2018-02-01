c59e5eb5b4e1aacfaedddef984ad1c8915ac0ed3
ianlikono <ianlikono@gmail.com>
2018-02-02 02:03:39 +0300

    added test with mocha

---
+++ test/Inbox.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const { interface, bytecode } = require('../compile');
+
+const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'Stopped'
+    }
+    drive() {
+        return 'Vroom'
+    }
+}
+
+let accounts;
+let inbox;
+
+beforeEach( async () => {
+    accounts = await web3.eth.getAccounts()
+
+    //deploy
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there']})
+        .send({ from: accounts[0], gas: '1000000'})
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(inbox)
+    })
+})
